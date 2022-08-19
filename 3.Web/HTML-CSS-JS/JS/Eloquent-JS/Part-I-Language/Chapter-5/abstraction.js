8004d8e558ec0a54ea2dbe2b580bf229c158800a
Yonathan <58274345+yonit1dev@users.noreply.github.com>
2022-08-19 10:42:54 +0300

    JS - CH6

---
+++ 3.Web/HTML-CSS-JS/JS/Eloquent-JS/Part-I-Language/Chapter-5/abstraction.js
@@ -0,0 +1,16 @@
+// Hiding lower details of implementation - abstraction.
+
+function repeat(n, action){
+    for(let i = 0; i < n; i++){
+        action(i);
+    }
+}
+
+repeat(3, console.log);
+
+let labels = [];
+repeat(5, i => {
+    labels.push(`Unit ${i + 1}`);
+});
+
+console.log(labels);
