cc429f91e01bbf3347b63dda443da5ff8b34b968
Yonathan <58274345+yonit1dev@users.noreply.github.com>
2022-08-26 14:05:22 +0300

    JS - AcademindJS - Finished Monster Killer

--- 3.Web/HTML-CSS-JS/JS/Academind-JS/3.Control-Structures/projects/assets/scripts/app.js
+++ 3.Web/HTML-CSS-JS/JS/Academind-JS/3.Control-Structures/projects/assets/scripts/app.js
@@ -1,17 +1,35 @@
 console.log("app.js connected!");
 
 const monsterHealth = document.getElementById("monster-bar");
+const monsterHealthText = document.getElementById("monster-health-text");
 const playerHealth = document.getElementById("player-bar");
+const playerHealthText = document.getElementById("player-health-text");
 const bonusLife = document.getElementById("bonus-life");
 
 const attackBtn = document.getElementById("attack");
 const strongAttackBtn = document.getElementById("strong-attack");
 const healBtn = document.getElementById("heal");
 const showLogBtn = document.getElementById("show-log");
+const resetBtn = document.getElementById("reset-game");
 
-function adjustHealthBars(maxLife){
-    monsterHealth.max = maxLife;
-    monsterHealth.value = maxLife;
-    playerHealth.max = maxLife;
-    playerHealth.value = maxLife;
+function adjustHealthBars(maxLife) {
+  monsterHealth.max = maxLife;
+  monsterHealth.value = maxLife;
+  playerHealth.max = maxLife;
+  playerHealth.value = maxLife;
+}
+
+function damageActor(damage, actor){
+    const damageDealt = Math.random() * damage;
+    actor.value = +actor.value - damageDealt;
+
+    return damageDealt;
+}
+
+function healPlayerHealth(healValue) {
+  playerHealth.value = +playerHealth.value + healValue;
+}
+
+function reset() {
+  adjustHealthBars(100);
 }
--- 3.Web/HTML-CSS-JS/JS/Academind-JS/3.Control-Structures/projects/assets/scripts/script.js
+++ 3.Web/HTML-CSS-JS/JS/Academind-JS/3.Control-Structures/projects/assets/scripts/script.js
@@ -1,6 +1,109 @@
 console.log("script.js connected!");
 
 const ATTACK_VALUE = 10;
-let chosenMaxLife = 50;
+const STRONG_ATTACK_VALUE = 16;
+const MONSTER_ATTACK_VALUE = 12;
+const HEAL_VALUE = 15;
 
-adjustHealthBars(chosenMaxLife);
+const enteredLife = prompt(
+  "Maximum life for you and the monster. Default is a 100."
+);
+
+let chosenMaxLife = parseInt(enteredLife);
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) chosenMaxLife = 100;
+
+let bonus_life = 1;
+let activeMonsterHealth = chosenMaxLife;
+let activePlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
+
+resetGame();
+
+function resetGame() {
+  bonusLife.textContent = +bonus_life;
+
+  activeMonsterHealth = chosenMaxLife;
+  activePlayerHealth = chosenMaxLife;
+
+  adjustHealthBars(chosenMaxLife);
+
+  setHealthText(monsterHealthText, activeMonsterHealth);
+  setHealthText(playerHealthText, activePlayerHealth);
+}
+
+function setHealthText(actor, value) {
+  actor.textContent = Math.round(value);
+}
+
+function checkWin() {
+  if (activeMonsterHealth <= 0 && activePlayerHealth > 0) {
+    alert("You've won!");
+    setHealthText(monsterHealthText, 0);
+    return;
+  } else if (activePlayerHealth <= 0 && activeMonsterHealth > 0) {
+    alert("You've lost!");
+    setHealthText(playerHealthText, 0);
+    return;
+  } else if (activeMonsterHealth <= 0 && activePlayerHealth <= 0) {
+    alert("A Draw!");
+    setHealthText(monsterHealthText, activeMonsterHealth);
+    setHealthText(playerHealthText, activePlayerHealth);
+    return;
+  }
+}
+
+function attackHandler(mode) {
+  if (mode === "attack") {
+    const damage = damageActor(ATTACK_VALUE, monsterHealth);
+    activeMonsterHealth -= damage;
+  } else if (mode === "strong") {
+    const damage = damageActor(STRONG_ATTACK_VALUE, monsterHealth);
+    activeMonsterHealth -= damage;
+  }
+
+  setHealthText(monsterHealthText, activeMonsterHealth);
+
+  checkWin();
+
+  const playerDamage = damageActor(MONSTER_ATTACK_VALUE, playerHealth);
+  activePlayerHealth -= playerDamage;
+  setHealthText(playerHealthText, activePlayerHealth);
+}
+
+function healHandler(healValue = 0) {
+  if (activePlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+    alert("You can't replenish more than your max health(100)!");
+    healValue = chosenMaxLife - activePlayerHealth;
+  } else {
+    healValue = HEAL_VALUE;
+  }
+
+  healPlayerHealth(healValue);
+  activePlayerHealth += healValue;
+  setHealthText(playerHealthText, activePlayerHealth);
+
+  checkWin();
+}
+
+function addLife() {
+  if (+bonusLife.textContent) {
+    healHandler(50);
+    bonusLife.textContent -= 1;
+  } else {
+    alert("You've no bonus life!");
+  }
+}
+
+attackBtn.addEventListener("click", () => {
+  attackHandler("attack");
+});
+
+strongAttackBtn.addEventListener("click", () => {
+  attackHandler("strong");
+});
+
+healBtn.addEventListener("click", healHandler);
+
+bonusLife.addEventListener("click", addLife);
+
+resetBtn.addEventListener("click", resetGame);
