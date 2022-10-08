a3bde57996ed5f68456caae92a46528aa7ff4189
yonit1dev <yonit.anosie@gmail.com>
2022-10-08 17:40:06 +0300

    JS - AcademindJS - Event Propagation

--- 3.Web/HTML-CSS-JS/JS/Academind-JS/13.Events/events.js
+++ 3.Web/HTML-CSS-JS/JS/Academind-JS/13.Events/events.js
@@ -2,27 +2,40 @@ const btn = document.querySelector("button");
 const anotherBtn = document.querySelector("button:last-of-type");
 const h2 = document.querySelector("h2");
 const scrollable = document.getElementById("scrollable");
+const div = document.querySelector("div");
 
 const btnClickHandler = () => {
   alert("Clicked");
 };
 // btn.onclick = btnClickHandler; // Can only be assigned once to an element.
 
-btn.addEventListener("click", btnClickHandler); // A better way to listen to events.
+btn.addEventListener("click", (event) => {
+  console.log("Clicked Button");
+  console.log(event);
+}); // A better way to listen to events.
 
-setTimeout(() => {
-  btn.removeEventListener("click", btnClickHandler);
-}, 3000);
+div.addEventListener(
+  "click",
+  (event) => {
+    console.log("Clicked DIV!");
+    console.log(event);
+  },
+  true
+);
 
 const anotherBtnClickHandler = (event) => {
+  event.stopPropagation(); // Does nothing to the default behavior of the element
   console.log(event);
   event.target.disabled = true;
 };
 
+// More types of event
 anotherBtn.addEventListener("mouseenter", anotherBtnClickHandler);
-h2.addEventListener("click", anotherBtnClickHandler); // To look at the target property of the event object.
+
+// To look at the target property of the event object.
+h2.addEventListener("click", anotherBtnClickHandler);
 
 // Scroll events
-window.addEventListener("scroll", (event) => {
-  console.log(event);
-});
+// window.addEventListener("scroll", (event) => {
+//   console.log(event);
+// });
