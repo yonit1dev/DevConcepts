6356b4c2fa0de9872acee8129e7ac8e7f86f37df
yonit1dev <yonit.anosie@gmail.com>
2022-10-14 11:02:21 +0300

    JS - AcademindJS - End of Numbers-Strings

--- 3.Web/HTML-CSS-JS/JS/Academind-JS/15.Numbers-Strings/strings.js
+++ 3.Web/HTML-CSS-JS/JS/Academind-JS/15.Numbers-Strings/strings.js
@@ -19,5 +19,5 @@ const email = "test@test.com";
 
 console.log(email.includes("@"));
 
-const regex = /^\S+@\S+\.\S+$/;
-console.log(regex.test(email));
+const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+console.log(emailRegex.test(email));
