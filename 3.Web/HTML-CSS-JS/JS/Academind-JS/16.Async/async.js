4b4c8eece3b83bab89162f3c1d94e2815e823ed7
yonit1dev <yonit.anosie@gmail.com>
2022-10-18 22:10:45 +0300

    JS - AcademindJS - Async Code

---
+++ 3.Web/HTML-CSS-JS/JS/Academind-JS/16.Async/async.js
@@ -0,0 +1,10 @@
+// Asynchronous Code
+
+const button = document.querySelector("button");
+const output = document.querySelector("p");
+
+function trackUserHandler() {
+  console.log("Clicked");
+}
+
+button.addEventListener("click", trackUserHandler);
