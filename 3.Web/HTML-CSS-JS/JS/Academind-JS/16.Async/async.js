5c9ba4e152a6aa31df37eb318041d81493398ff0
yonit1dev <yonit.anosie@gmail.com>
2022-10-19 11:54:40 +0300

    JS - AcademindJS - More Examples on async and sync code running

--- 3.Web/HTML-CSS-JS/JS/Academind-JS/16.Async/async.js
+++ 3.Web/HTML-CSS-JS/JS/Academind-JS/16.Async/async.js
@@ -4,14 +4,28 @@ const button = document.querySelector("button");
 const output = document.querySelector("p");
 
 function trackUserHandler() {
-  console.log("Clicked");
+  navigator.geolocation.getCurrentPosition(
+    (posData) => {
+      setTimeout(() => {
+        console.log(posData);
+      }, 2000);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+  setTimeout(() => {
+    console.log("Timer Done!");
+  }, 0);
+  
+  console.log("Getting location..."); // this executes first before the fetch of location, since the latter is handed off to the browser for execution.
 }
 
 button.addEventListener("click", trackUserHandler);
 
-let result = 0;
-for (let i = 0; i < 100000000; i++) {
-  result += i;
-}
+// let result = 0;
+// for (let i = 0; i < 100000000; i++) {
+//   result += i;
+// }
 
-console.log(result); // the result is logged first, even if the button is clicked before calculation.
+// console.log(result); // the result is logged first, even if the button is clicked before calculation.
