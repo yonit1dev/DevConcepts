cf8cdf888cbe565e946e07c56bfc788ac7a9b7c8
yonit1dev <yonit.anosie@gmail.com>
2022-10-25 11:25:46 +0300

    JS - AcademindJS - HTTP - new http methods learnt - error handling learnt

--- 3.Web/HTML-CSS-JS/JS/Academind-JS/17.Http/starting-file/scripts/http.js
+++ 3.Web/HTML-CSS-JS/JS/Academind-JS/17.Http/starting-file/scripts/http.js
@@ -1,23 +1,88 @@
-const xhr = new XMLHttpRequest();
 const listElement = document.querySelector(".posts");
 const postTemplate = document.querySelector("template");
+const form = document.querySelector("form");
+const postTitle = document.getElementById("title");
+const postContent = document.getElementById("content");
+const addPostBtn = document.querySelector(".form-control button");
+const fetchPostBtn = document.querySelector("#available-posts button");
+const postList = document.querySelector("ul");
 
-// Data Transfer
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts"); // configuring the request
-xhr.responseType = "json";
+function sendRequests(method, url, data) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-xhr.addEventListener("load", function () {
-  const fetchedPosts = xhr.response.slice(0, 15);
-  console.log(fetchedPosts);
+    // Data Transfer
+    xhr.open(method.toString(), url.toString()); // configuring the request
+    xhr.responseType = "json";
 
-  // Rendering Posts
+    xhr.addEventListener("load", function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(new Error("Server error! Try again!"));
+      }
+    });
+
+    xhr.addEventListener("error", function () {
+      reject(new Error("Failed to send request!"));
+    });
+
+    xhr.send(JSON.stringify(data));
+  });
+
+  return promise;
+}
+
+async function fetchPosts() {
+  let response = await sendRequests(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  let fetchedPosts = response.slice(0, 15);
+
   for (const post of fetchedPosts) {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector("h2").textContent = post.title.toUpperCase();
     postEl.querySelector("p").textContent = post.body;
+    postEl.querySelector("li").id = post.id;
 
     listElement.append(postEl);
   }
+}
+
+async function createPost(title, content) {
+  const userId = Math.floor(Math.random() * 10);
+  const body = {
+    title: title,
+    body: content,
+    userId: userId,
+  };
+
+  await sendRequests(
+    "POST",
+    "https://jsonplaceholder.typicode.com/posts",
+    body
+  );
+}
+
+fetchPostBtn.addEventListener("click", () => {
+  fetchPosts();
+});
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // prevent the browser from submitting the form
+  const title = postTitle.value;
+  const content = postContent.value;
+
+  createPost(title, content);
 });
 
-xhr.send();
+postList.addEventListener("click", (event) => {
+  if (event.target.id === "delete-btn") {
+    const deletedID = event.target.closest("li").id;
+    sendRequests(
+      "DELETE",
+      `https://jsonplaceholder.typicode.com/posts/${deletedID}`
+    );
+  }
+});
