8a151d5f99e2f0ed76a68269a5db9c0f0bba8429
yonit1dev <yonit.anosie@gmail.com>
2022-11-01 12:23:56 +0300

    JS - ColtSteeleJS - Miscellaneous - Changes to spread.js

--- 3.Web/HTML-CSS-JS/JS/ColtSteele-JS/11.miscellaneous/spread.js
+++ 3.Web/HTML-CSS-JS/JS/ColtSteele-JS/11.miscellaneous/spread.js
@@ -9,15 +9,6 @@ const dog = { ...canine, isPet: true };
 
 console.log(dog);
 
-function addManyNumbers(...nums) {
-  let sum = 0;
-  for (let num of nums) {
-    sum += +num;
-  }
-
-  return sum;
-}
-
 console.log(addManyNumbers(1, 2));
 
 const nums = [45, 23, 34, 7, 5];
