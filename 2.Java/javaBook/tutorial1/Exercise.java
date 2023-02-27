43bf23099434c5d66dea10eb4df60e5071421ab5
yonit1dev <yonit.anosie@gmail.com>
2023-02-27 09:25:50 +0300

    Java tutorials

---
+++ 2.Java/javaBook/tutorial1/Exercise.java
@@ -0,0 +1,22 @@
+package javaBook.tutorial1;
+
+public class Exercise {
+    public static void main(String[] args) {
+        // printing name, address and telephone number
+        System.out.println("Name: Yonathan Tessema.");
+
+        // with blank line exercise
+        System.out.println("Address: Addis Ababa, Ethiopia.\n");
+        System.out.println("Tel: +251900000000");
+
+        // displaying initials using  asteriks
+        System.out.println("*        *     * * * * * * * * *");
+        System.out.println(" *      *              *");
+        System.out.println("  *    *               *");
+        System.out.println("   * *                 *");
+        System.out.println("    *                  *");
+        System.out.println("    *                  *");
+        System.out.println("    *                  *");
+        System.out.println("    *                  *");
+    }
+}
---
+++ 2.Java/javaBook/tutorial1/Hello.java
@@ -0,0 +1,12 @@
+package javaBook.tutorial1;
+
+public class Hello {
+    public static void main(String[] args) {
+        // first program in java
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Again!");
+
+        System.out.print("Hello, with no line!");
+        System.out.print("No line!");
+    }
+}
